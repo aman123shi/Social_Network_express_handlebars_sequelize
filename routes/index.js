d6c5a2dd516f5775f29cfa30f8eef8ffdc62b58e
Amanuel Shiferaw <123aman64@gmail.com>
2022-07-07 22:57:36 +0300

    Add mapping To Routes

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+//importing Routes
 var homerouter = require("./homerouter");
 var chatrouter = require("./chatrouter");
 var messageRouter = require("./messagerouter");
 var friendsRouter = require("./friendsrouter");
 var profileRouter = require("./profileroutes");
 var notificationRouter = require("./notificationroutes");
-router.use('/', homerouter);
+//mapping Routes
+router.use("/", homerouter);
 router.use("/chat", chatrouter);
 router.use("/messages", messageRouter);
 router.use("/friends", friendsRouter);
 router.use("/profile", profileRouter);
 router.use("/notifications", notificationRouter);
 router.get("/logout", (req, res) => {
-    res.clearCookie("mysite");
-    res.redirect("/");
+  res.clearCookie("mysite");
+  res.redirect("/");
 });
 module.exports = router;
