4d29e88fa4044f296137b157a0c75c53cf5b8e71
Amanuel Shiferaw <123aman64@gmail.com>
2022-07-07 22:53:39 +0300

    Add field sender and Fix some datatypes

--- models/message.js
+++ models/message.js
@@ -1,28 +1,32 @@
-module.exports=(sequelize,DataType)=>{
-    const Message= sequelize.define("Messages" ,{
-          id:{
-              type:DataType.INTEGER,
-              primaryKey:true,
-              autoIncrement:true
-          },
-            sender:{
-                type:DataType.INTEGER,
-                allowNull:false,
-                validate:{
-                  notEmpty:true  
-                }
-            },
-            content:{
-                type:DataType.STRING,
-                allowNull:false,
-                validate:{
-                  notEmpty:true  
-                }
-            }
-          
-    }
-    
-    );
-  
-    return Message;
-}
+module.exports = (sequelize, DataType) => {
+  const Message = sequelize.define("Messages", {
+    id: {
+      type: DataType.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    sender: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    receiver: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    content: {
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  });
+
+  return Message;
+};
